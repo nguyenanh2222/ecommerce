3191382bee54d400ffc28a4625ea60734d66a202
nguyenanh2222 <nguyenvieanh2103@gmail.com>
2022-06-17 14:29:35 +0700

    make api analysis for admin

--- admin/analysis.py
+++ admin/analysis.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter, Query, Body, Path
 from starlette import status
 
 from database import SessionLocal
@@ -7,16 +7,12 @@ from project.core.schemas import PageResponse, Sort, DataResponse
 from project.core.swagger import swagger_response
 from datetime import datetime, timezone
 from customer.order import OrderRes
-
+from sqlalchemy.engine import CursorResult
 router = APIRouter()
 
 
-class CusorResult:
-    pass
-
-
 @router.get(
-    path="/analysis",
+    path="/",
     status_code=status.HTTP_200_OK,
     description= "tính doanh thu theo một khoảng thời gian",
     responses=swagger_response(
@@ -27,7 +23,8 @@ class CusorResult:
 
 async def analysis_revenue_in_period(
         start_datetime: datetime = Query(datetime.strptime("2021-11-29", "%Y-%m-%d")),
-        end_datetime: datetime = Query(datetime.strptime("2021-11-29", "%Y-%m-%d"))
+        end_datetime: datetime = Query(datetime.strptime("2021-11-29", "%Y-%m-%d")),
+        product_id: int = Query(None)
 ):
 
     session = SessionLocal()
@@ -35,8 +32,9 @@ async def analysis_revenue_in_period(
     query = f"""SELECT SUM(total_amount), AVG(total_amount) 
     FROM ecommerce.orders 
     WHERE time_open >= '{start_datetime}' 
-    AND time_open <= '{end_datetime}'"""
-    _rs: CusorResult = session.execute(query)
+    AND time_open <= '{end_datetime}'
+    """
+    _rs: CursorResult = session.execute(query)
     revenue = _rs.fetchone()
     return DataResponse(data= revenue)
 
